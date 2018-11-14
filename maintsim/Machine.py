135b9efeff694a0cb251bff6ea4f5ae72a5c08bb
m-hoff <MichaelHoffman@psu.edu>
2018-11-14 17:21:22 -0500

    Added simulation method. Started data collection.

--- maintsim/Machine.py
+++ maintsim/Machine.py
@@ -5,142 +5,178 @@ from Globals import G
 
 class Machine:
     def __init__(self, 
-				 env, 
-				 m, 
-				 process_time, 
-				 degradation, 
-				 planned_failures,
-				 system, 
-				 repairman):
-				 
-		self.env = env
-		self.m = m
-		self.process_time = process_time
-		self.degradation = degradation
-		self.planned_failures = planned_failures
-		self.system = system
-		self.repairman = repairman
-		
-		# determine maintenance policy for machine
-		self.maintenance_policy = self.system.maintenance_policy
-		maintenance_parameters = self.system.maintenance_parameters
-		if self.maintenance_policy == 'PM':
-			self.PM_interval = maintenance_parameters['PM interval'][self.m]
-			self.PM_duration = maintenance_parameters['PM duration'][self.m]
-		elif self.maintenance_policy == 'CBM':
-			self.CBM_threshold = maintenance_parameters['CBM threshold'][self.m]
-		
-		# assign input buffer
-		if self.m > 0:
-			self.in_buff = self.system.buffers[self.m-1]
-			
-		# assign output buffer
-		if (self.m < self.system.M-1):
-			self.out_buff = self.system.buffers[m]
-		
-		# set initial machine state
-		# maintenance state
-		self.health = 0 # starts in perfect health
-		self.awaiting_maintenance = False
-		self.failed = False
-		self.repair_type = None
-		# production state
-		self.has_part = False
-		self.parts_made = 0
-		
-	def working(self, repairman):
-		'''
-		Main production function. Machine will produce parts
-		until interrupted by failure. 
-		'''
-		prev_part = 0
-		while True:
-			try:
-				idle_start = idle_stop = 0
-				
-				# get part from input buffer
-				if self.m > 0: 
-					idle_start = self.env.now - G.warmup_time
-					yield self.in_buff.get(1)
-					idle_stop = self.env.now - G.warmup_time
-					self.has_part = True
-					self.remaining_process_time = self.process_time
-					
-				# check if machine was starved
-				if idle_stop - idle_start > 0:
-					# TODO: record idle time
-					pass
-					
-				# process part
-				for t in range(self.process_time):
-					# TODO: record processing
-					yield self.env.timeout(1)
-					self.remaining_process_time -= 1
-					
-				# put finished part in output buffer
-				idle_start = idle_stop = 0
-				if self.m < self.system.M-1:
-					idle_start = self.env.now - G.warmup_time
-					yield self.out_buff.put(1)
-					idle_stop = self.env.now - G.warmup_time
-					self.has_part = False
-					
-				# check if machine was blocked
-				if idle_stop - idle_start > 0:
-					# TODO: record idle time
-					pass
-					
-				if self.env.now > G.warmup_time:
-					self.parts_made += 1
-					# TODO: record parts made
-					prev_part = self.env.now
-			
-			except simpy.Interrupt: 
-				# processing interrupted due to failure
-				self.broken = True
-				self.has_part = False
-				
-				# TODO: fix this
-				time_to_repair = 10
-				
-				with repairman.request(priority=1) as req:
-					yield req
-					yield self.env.timeout(time_to_repair)
-					# TODO: record maintenance data
-				
-				self.health = 0
-				self.broken = False
-				
-				# TODO: record more maintenance data
-	
-	def degrade(self):
-		'''
-		Discrete state degradation process. 
-		'''
-		while True:
-			while random() > self.degradation:
-				# do NOT degrade
-				yield self.env.timeout(1)
-				# TODO: check planned failures
-			
-			# degrade by one unit once loop breaks
-			yield self.env.timeout(1)
-			
-			if self.health < 10:
-				# machine is NOT failed
-				self.health += 1
-				
-				if self.health == 10: # machine fails
-					self.broken = True
-					self.repair_type = 'CM'
-					self.process.interrupt()
-					
-				elif (self.maintenance_policy == 'CBM') and (self.health == self.CBM_threshold):
-					# TODO schedule preventive maintenance
-					self.repair_type = 'CBM'
-					# self.process.interrupt() (?)
-					pass
-					
-			
-			
-		
+                 env, 
+                 m, 
+                 process_time, 
+                 degradation, 
+                 planned_failures,
+                 system, 
+                 repairman):
+                 
+        self.env = env
+        self.m = m
+        self.name = 'M{}'.format(self.m)
+        self.process_time = process_time
+        self.degradation = degradation
+        self.planned_failures = planned_failures
+        self.system = system
+        self.repairman = repairman
+        
+        # determine maintenance policy for machine
+        self.maintenance_policy = self.system.maintenance_policy
+        maintenance_parameters = self.system.maintenance_params
+        if self.maintenance_policy == 'PM':
+            self.PM_interval = maintenance_parameters['PM interval'][self.m]
+            self.PM_duration = maintenance_parameters['PM duration'][self.m]
+        elif self.maintenance_policy == 'CBM':
+            self.CBM_threshold = maintenance_parameters['CBM threshold'][self.m]
+        
+        # assign input buffer
+        if self.m > 0:
+            self.in_buff = self.system.buffers[self.m-1]
+            
+        # assign output buffer
+        if (self.m < self.system.M-1):
+            self.out_buff = self.system.buffers[m]
+        
+        # set initial machine state
+        # maintenance state
+        self.health = 0 # starts in perfect health
+        self.awaiting_maintenance = False
+        self.failed = False
+        self.repair_type = None
+        # production state
+        self.has_part = False
+        self.parts_made = 0
+        
+        self.process = self.env.process(self.working(self.repairman))
+        self.env.process(self.degrade())
+        
+    def working(self, repairman):
+        '''
+        Main production function. Machine will produce parts
+        until interrupted by failure. 
+        '''
+        prev_part = 0
+        while True:
+            try:
+                idle_start = idle_stop = 0
+                
+                # get part from input buffer
+                if self.m > 0: 
+                    idle_start = self.env.now - G.warmup_time
+                    yield self.in_buff.get(1)
+                    idle_stop = self.env.now - G.warmup_time
+                    self.has_part = True
+                
+                self.remaining_process_time = self.process_time
+                    
+                # check if machine was starved
+                if idle_stop - idle_start > 0:
+                    print('M{} idle at time {}'.format(self.m, self.env.now))
+                    self.system.machine_data.loc[idle_start:idle_stop, 
+                                                 self.name+' forced idle'] = 1
+                    
+                # process part
+                for t in range(self.process_time):
+                    # TODO: record processing
+                    yield self.env.timeout(1)
+                    self.remaining_process_time -= 1
+                    
+                # put finished part in output buffer
+                idle_start = idle_stop = 0
+                if self.m < self.system.M-1:
+                    idle_start = self.env.now - G.warmup_time
+                    yield self.out_buff.put(1)
+                    idle_stop = self.env.now - G.warmup_time
+                    self.has_part = False
+                    
+                # check if machine was blocked
+                if idle_stop - idle_start > 0:
+                    self.system.machine_data.loc[idle_start:idle_stop, 
+                                                 self.name+' forced idle'] = 1
+                    
+                if self.env.now > G.warmup_time:
+                    self.parts_made += 1
+                    # TODO: record parts made
+                    prev_part = self.env.now
+            
+            except simpy.Interrupt: 
+                # processing interrupted due to failure
+                self.broken = True
+                self.has_part = False
+                
+                # TODO: fix this
+                time_to_repair = 10
+                
+                # TODO: get priority
+                with repairman.request(priority=1) as req:
+                    yield req
+                    yield self.env.timeout(time_to_repair)
+                    # TODO: record maintenance data
+                
+                self.health = 0
+                self.broken = False
+                
+                # TODO: record more maintenance data
+    
+    def degrade(self):
+        '''
+        Discrete state degradation process. 
+        '''
+        while True:
+            # TODO: incorporate markov chain degradation
+            while random() > self.degradation:
+                # do NOT degrade
+                yield self.env.timeout(1)
+                # TODO: check planned failures
+                for failure in self.planned_failures:
+                    if failure[1] == self.env.now:
+                        # break
+                        self.process.interrupt()
+            
+            # degrade by one unit once loop breaks
+            yield self.env.timeout(1)
+            
+            if self.health < 10:
+                # machine is NOT failed
+                self.health += 1
+                
+                if self.health == 10: # machine fails
+                    self.broken = True
+                    self.repair_type = 'CM'
+                    self.process.interrupt()
+                    
+                elif (self.maintenance_policy == 'CBM') and (self.health >= self.CBM_threshold):
+                    # TODO schedule preventive maintenance
+                    self.repair_type = 'CBM'
+                    if self.repairman.count() == 0:
+                        # only interrupt processing if repairman available
+                        self.process.interrupt()
+                    
+    def write_data(self):
+        '''
+        Record all real-time state data at each time step.
+        '''
+        t = self.env.now
+        
+        # machine status
+        if self.has_part:
+            self.system.state_data.loc[t, self.name+' has_part'] = 1
+        else:
+            self.system.state_data.loc[t, self.name+' has_part'] = 0
+        
+        self.system.production_data.loc[t, self.name+' production'] = self.parts_made
+        
+        if self.failed:
+            self.system.machine_data.loc[t, self.name+' functional'] = 0
+        else:
+            self.system.machine_data.loc[t, self.name+' functional'] = 1
+            
+        
+        
+        # buffer status
+        
+        # queue status
+            
+        
--- maintsim/System.py
+++ maintsim/System.py
@@ -1,6 +1,8 @@
 import simpy
+import time
+import pandas as pd
 
-import Machine
+from Machine import Machine
 # import Buffer
 # import Source
 # import Sink
@@ -16,77 +18,121 @@ class System:
 
                  failures={'degradation':None, # list 
                            'reliability':None}, # scipy distributions
-				 degradation=None, # list or int of degradation rate
-				 planned_failures=None, # list of (loc, time, duration)
+                 degradation=None, # list or int of degradation rate
+                 planned_failures=None, # list of (loc, time, duration)
                  maintenance_policy=None, # CM/PM/CBM, str
                  maintenance_params=None, # define policy
-				 maintenance_capacity=1,
+                 maintenance_capacity=1,
                  maintenance_costs=None):
 
-		# specified system characteristics
+        # specified system characteristics
         self.process_times = process_times
         self.interarrival_time = interarrival_time
-        if type(self.buffer_sizes) == int:
-			self.buffer_sizes = [buffer_sizes]*(len(process_times)-1)
-		else:
-			self.buffer_sizes = buffer_sizes
-		self.failures = failures	
-		if type(self.degradation) == int:
-			self.degradation = [degradation]*(len(process_times))
-		else:
-			self.degradation = degradation
-		self.planned_failures = planned_failures
+        if type(buffer_sizes) == int:
+            self.buffer_sizes = [buffer_sizes]*(len(process_times)-1)
+        else:
+            self.buffer_sizes = buffer_sizes
+        self.failures = failures    
+        #if type(degradation) == int:
+        #    self.degradation = [degradation]*(len(process_times))
+        #else:
+        #    self.degradation = degradation
+        if degradation:
+            self.degradation = degradation
+        else:
+            self.degradation = [0]*len(process_times)
+        
+        self.planned_failures = planned_failures
         self.maintenance_policy = maintenance_policy
-		self.maintenance_params = maintenance_params
-		self.maintenance_capacity = maintenance_capacity
+        self.maintenance_params = maintenance_params
+        self.maintenance_capacity = maintenance_capacity
         self.maintenance_costs = maintenance_costs
 
-		# inferred system characteristics
-		self.M = len(process_times) # number of machines
-		self.bottleneck_rate = max(self.process_times)
-		self.bottleneck = self.process_times.index(bottleneck_rate)
-		
+        # inferred system characteristics
+        self.M = len(process_times) # number of machines
+        self.bottleneck_rate = max(self.process_times)
+        self.bottleneck = self.process_times.index(self.bottleneck_rate)
+        
         self.initialize() # initialize system objects
-		
-		# simulation parameters
-		self.warmup_time = 0
-		
-	def initialize():
-		# create simpy environment
-		self.env = simpy.Environment()
-	
-		# create repairman object
-		self.repairman = simpy.PriorityResource(self.env, capacity=self.maintenance_capacity)
-	
-		# create source object
-	
-		# create objects for each machine
-		self.buffers = []
-		self.machines = []
-		
-		for m in range(self.M):
-			# buffer objects
-			if m < (self.M - 1):
-				self.buffers += [simpy.Container(self.env, capacity=self.buffer_sizes[m])]
-			
-			# planned failures for m
-			planned_falures_m = [DT for DT in self.planned_failures if DT[0] == m]
-			
-			# machine objects
-			process_time = self.process_times[m]
-			self.machines += [Machine(self.env, m, process_time, degradation[m],
-									  planned_failures_m, self, self.repairman)]
+        
+        # simulation parameters
+        self.warmup_time = 0
+        
+    def initialize(self):
+        '''
+        Prepares the system for simulation. The system must be 
+        reinitialized with a new simpy environment for each 
+        iteration of the simulation. 
+        '''
+    
+        # create simpy environment
+        self.env = simpy.Environment()
+    
+        # create repairman object
+        self.repairman = simpy.PriorityResource(self.env, capacity=self.maintenance_capacity)
+    
+        # create source object
+    
+        # create objects for each machine
+        self.buffers = []
+        self.machines = []
+        
+        for m in range(self.M):
+            # buffer objects
+            if m < (self.M - 1):
+                self.buffers += [simpy.Container(self.env, capacity=self.buffer_sizes[m])]
+            
+            # planned failures for m
+            if self.planned_failures:
+                planned_falures_m = [DT for DT in self.planned_failures if DT[0] == m]
+            else:
+                planned_failures_m = []
+            
+            # machine objects
+            process_time = self.process_times[m]
+            self.machines += [Machine(self.env, m, process_time, self.degradation[m],
+                                      planned_failures_m, self, self.repairman)]
 
-									  
-		# initialize system data collection							  
-		
-		
-									  
-									  
-									  
-									  
-									  
-									  
-									  
-									  
-									  
+                                      
+        # initialize system data collection
+        state_cols = ['time']     # system state data
+        prod_cols = ['time']      # production data
+        machine_cols = ['time']   # machine status data
+        
+        
+        for machine in self.machines:
+            state_cols += [machine.name + ' has part']
+            if machine.m < (self.M - 1):
+                state_cols += ['b{} level'.format(machine.m)]
+                
+            prod_cols += [machine.name+' production', machine.name+' throughput']
+                
+            machine_cols += [machine.name+' functional', machine.name+' forced idle']
+            
+            
+        self.state_data = pd.DataFrame(columns=state_cols)
+        self.production_data = pd.DataFrame(columns=prod_cols)
+        self.machine_data = pd.DataFrame(columns=machine_cols)        
+        self.queue_data = pd.DataFrame(columns=['time', 'contents'])
+        self.maintenance_data = pd.DataFrame(columns=['time',
+                                                      'machine',
+                                                      'type',
+                                                      'activity',
+                                                      'duration'])
+        
+    def simulate(self, title='Simulation',
+                 warmup_time=0,
+                 sim_time=100,
+                 seed=None,
+                 verbose=True):
+        
+        start_time = time.time()
+        if seed:
+            random.seed(seed)
+            
+        self.initialize()
+        
+        self.env.run(until=warmup_time+sim_time)   
+
+        if verbose:
+            print('Simulation complete in {:.2f}s'.format(time.time()-start_time))                                               
