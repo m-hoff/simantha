2709dfe4acf442e60ea78b0dd7338cdd073f960e
m-hoff <MichaelHoffman@psu.edu>
2020-03-03 17:10:49 -0500

    Daily commit

--- simantha/_Machine.py
+++ simantha/_Machine.py
@@ -5,13 +5,14 @@ import simpy
 
 from utils import *
 
-class _Machine(simpy.Resource):
+#class _Machine(simpy.Resource):
+class _Machine:
     def __init__(
         self, 
         env=None, 
-        capacity=1,
 
         cycle_time=1,
+        capacity=1,
 
         failed_state=10,
         degradation_rate=0,
@@ -20,16 +21,17 @@ class _Machine(simpy.Resource):
         initial_health=0,
         initial_remaining_process=None
     ):
-        super().__init__(env, capacity)
+        #super().__init__(env, capacity)
 
         # machine configuration parameters
         self.cycle_time = cycle_time
-        
+        self.capacity = capacity
+
         if (degradation_rate is not None) and (degradation_matrix is not None):
-            warnings.warn((
+            warnings.warn(
                 'Degradation rate and failed state will be overwritten by '
                 'specified degradation matrix.'
-            ))
+            )
             self.degradation_matrix = degradation_matrix
             self.failed_state = len(degradation_matrix) - 1
         else:
@@ -43,11 +45,17 @@ class _Machine(simpy.Resource):
         self.initial_remaining_process = initial_remaining_process
 
     def initialize(self, env):
-        self._env = env
+        self.env = env
+
+        # TODO: verify naming, functionality, etc. 
+        self.contents = simpy.Container(env, self.capacity)
 
         # intialize machine attributes
         self.health = self.initial_health
         self.remaining_process = self.initial_remaining_process
+
+        self.finished_parts = 0
+        self.work_in_process = 0
 
         # initialize performance statistics
         self.parts_made = 0
@@ -55,9 +63,13 @@ class _Machine(simpy.Resource):
     def production(self):
         while True:
             try:
-                if self.upstream:
-                    for obj in self.upstream:
-                        pass
+                # get part from upstream object
+                #TODO: assign priority to upstream objects
+                for upstream_object in self.upstream:
+                    if upstream_object.contents.level:
+                        yield upstream_object.contents.get(1)
+                        self.contents.put(1)
+
+                
             except:
                 pass
-
