4da1ad0b654223969fee1d45571fb3d978f9708f
Michael Hoffman <MichaelHoffman@psu.edu>
2020-12-20 11:41:52 -0500

    started putting examples together

--- simantha/__init__.py
+++ simantha/__init__.py
@@ -1,14 +1,15 @@
-from .System import *
+from .System import System
 from .Source import *
 from .Machine import *
 from .Buffer import *
 from .Sink import *
 from .Maintainer import *
+from .simulation import *
 from .utils import *
 
-__name__ = 'simantha'
+#__name__ = 'simantha'
 
-__all__ = ['System', 'Maintainer', 'Machine']
+#__all__ = ['System', 'Maintainer', 'Machine']
 
-__title__ = 'simantha'
-__author__ = 'Michael Hoffman <hoffman@psu.edu>'
+#__title__ = 'simantha'
+#__author__ = 'Michael Hoffman <hoffman@psu.edu>'
---
+++ simantha/examples/SingleMachine.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../..'))
+
+from simantha import Source, Machine, Sink, System
+
+def main():
+    source = Source()
+    M1 = Machine(name='M1', cycle_time=1)
+    sink = Sink()
+
+    source.define_routing(downstream=[M1])
+    M1.define_routing(upstream=[source], downstream=[sink])
+    sink.define_routing(upstream=[M1])
+
+    system = System(objects=[source, M1, sink])
+
+    system.simulate(simulation_time=100)
+
+if __name__ == '__main__':
+    main()
+    
